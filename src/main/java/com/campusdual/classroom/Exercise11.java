ea93f57778c13bde76d9bea518a32f2629a04e36
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-25 12:11:39 +0200

    Ejercicio 11 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -2,10 +2,42 @@ package com.campusdual.classroom;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        TVRemote redRemote = new TVRemote("Rojo");
-        TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
-    }
 
+        TVRemote remote = new TVRemote("Rojo");
+
+
+        remote.turnOn();
+        System.out.println("TV Encendida: " + remote.on);
+
+
+        remote.channelUp();
+        System.out.println("Canal: " + remote.channel);
+
+
+        remote.channelDown();
+        System.out.println("Canal: " + remote.channel);
+
+
+        remote.channel = 0;
+        remote.channelDown();
+        System.out.println("Canal después de intentar bajar: " + remote.channel);
+
+
+        remote.volumeUp();
+        System.out.println("Volumen: " + remote.volume);
+
+
+        remote.volumeDown();
+        System.out.println("Volumen después de bajar: " + remote.volume);
+
+
+        remote.volume = 0;
+        remote.volumeDown();
+        System.out.println("Volumen después de intentar bajar: " + remote.volume);
+
+        System.out.println("Color del mando a distancia: " + remote.getColor());
+
+        remote.turnOff();
+        System.out.println("TV Apagada: " + remote.on);
+    }
 }
